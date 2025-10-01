9ae5f93e8579e8e296b0d76dd35ce022d0633a78
Mohammedamin Kheiru <89771645+mamine27@users.noreply.github.com>
2025-10-01 15:12:22 +0300

    Created 'A - Frog 1' by A2SV Hub

---
+++ 01-Oct-2025/A - Frog 1 398512.py
@@ -0,0 +1,111 @@
+# Problem: A - Frog 1 - https://atcoder.jp/contests/dp/tasks/dp_a
+
+from heapq import heappush, heappop
+from collections import defaultdict, deque
+from random import randint    
+from cmath import inf
+from math import ceil
+rand = randint(1000,10000000)
+from sys import stdin
+ 
+def ii(): return int(stdin.readline().strip())
+def ll(): return list(map(int, stdin.readline().strip().split()))
+def ss() : return stdin.readline().strip()
+
+def Counter(arr , asked = None):
+    cnt = defaultdict(int)   
+    for i in arr:
+        cnt[i^rand] += 1
+    if asked:
+        return cnt[asked ^ rand]
+    
+    return cnt
+
+def sieve():
+    mx = 10 ** 6 + 2
+    prm = [0]*(mx) 
+    for i in range(2,mx):
+        if prm[i]:
+            continue
+        for j in range(i,mx,i):
+            prm[j] += 1
+
+
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+class UnionFind:
+    def __init__(self):
+        self.size = {}
+        self.root = {}
+
+    def find(self, x):
+        if x not in self.root:
+            self.root[x] = x
+            self.size[x] = 1
+            return x
+
+        while x != self.root[x]:
+            x = self.root[self.root[x]]
+
+        return x
+
+    def union(self, x, y):
+        repx = self.find(x)
+        repy = self.find(y)
+        if repx == repy:
+            return
+
+        szx = self.size[repx]
+        szy = self.size[repy]
+
+        if szx > szy:
+            self.root[repy] = self.root[repx]
+            self.size[repx] += self.size[repy]
+           
+        else:
+            self.root[repx] = self.root[repy]
+            self.size[repy] += self.size[repx]
+    
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# nnnnnnnnnnnnnnnnnnnnnnnnnn
+def solve():
+    n = ii()
+    arr = ll()
+
+    dp = [inf] * (n)
+    dp[0] = 0
+    for i in range(n-1):
+        dp[i+1] = min(dp[i+1] , dp[i] + abs(arr[i+1] - arr[i]))
+        if i + 2 < n:
+            dp[i+2] = min(dp[i+2] , dp[i] + abs(arr[i+2] - arr[i]))
+    
+    return dp[-1]
+
+ 
+tt = 1
+# tt = ii()
+res = []
+for _ in range(tt):
+    res.append(solve())
+    
+for i in res:
+    print(i)
+ 
+
