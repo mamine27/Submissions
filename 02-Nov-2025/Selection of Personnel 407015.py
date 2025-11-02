7f82a074746930bbc7341bfb1888e6d1d0486aa2
Mohammedamin Kheiru <89771645+mamine27@users.noreply.github.com>
2025-11-02 22:07:34 +0300

    Created 'Selection of Personnel' by A2SV Hub

---
+++ 02-Nov-2025/Selection of Personnel 407015.py
@@ -0,0 +1,25 @@
+# Problem: Selection of Personnel - https://codeforces.com/problemset/problem/630/F
+
+from heapq import heappush, heappop
+from collections import defaultdict, deque
+from random import randint
+from cmath import inf, sqrt
+from math import ceil, comb, gcd, isqrt, lcm
+from sys import stdin
+
+def ii(): return int(stdin.readline().strip())
+def ll(): return list(map(int, stdin.readline().strip().split()))
+def ss(): return stdin.readline().strip()
+rand = randint(1000, 10000000)
+
+def solve():
+    n = ii()
+    ans =0 
+    ans += comb(n,5)
+    ans += comb(n,6)
+    ans += comb(n,7)
+
+    return ans
+
+print(solve())
+
